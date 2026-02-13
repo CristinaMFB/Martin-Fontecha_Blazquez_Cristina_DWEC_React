b15ff5d1c70533b66aa5bdbe8f2ac99fc303948c
CristinaMFB <3370337@murciaeduca.es>
2026-02-13 16:55:05 +0100

    Añadida parte del HTML con la tabla para la predicción por días y funciones necesarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
+/*Función para obtener un icono segun la descripción del estado del cielo, me gusta así porque lo hace más visual
+y casi todas las aplicaciones y webs del tiempo lo tienen.*/
+function iconoDescripcion(descripcion) {
+   //En el caso de que no haya descripción, dejar en blanco
+  if(!descripcion) return " ";
+
+  //Pasar toda la descripcion a minúsculas y almacenar en texto
+  const texto = descripcion.toLowerCase();
+
+  //Tormenta
+  if(texto.includes("tormenta")) return "⛈️";
+
+  //Nieve
+  if(texto.includes("nieve")) return "❄️";
+
+  //Lluvia, llovizna o chubascos
+  //Pongo "lluvi" para que detecte lluvia, lluvias y lluvioso
+  if (texto.includes("lluvi") || texto.includes("chubasco") || texto.includes("llovizna")) return "🌧️";
+
+  //Niebla, bruma o calima
+  if (texto.includes("niebla") || texto.includes("bruma") || texto.includes("calima")) return "🌫️";
+
+  //Parcialmente soleado
+  if (texto.includes("parcialmente soleado")) return "🌤️";
+
+  //Poco nuboso, intervalos nubosos o nubes altas
+  if (texto.includes("poco nuboso") || texto.includes("intervalos nubosos") || texto.includes ("nubes altas")) return "⛅";
+
+  //Nuboso, muy nuboso, cubierto
+  if(texto.includes("nuboso") || texto.includes ("muy nuboso") || texto.includes("cubierto")) return "⛅";
+
+  //Soleado, despejado
+  if(texto.includes("despejado")) return "☀️";
+
+  //En el caso de que no coincida ninguna descripción, dejar en blanco
+  return " ";
+}
+
 function App() {
   //useState para guardar variables
   //Guardar las selecciones del usuario en el formulario
@@ -14,8 +52,9 @@ function App() {
 
   //Predicción diaria y por horas
   const [prediccionDiaria, setPrediccionDiaria] = useState(null);
-  const [prediccionHoraria, setPrediccionHoraria] = useState(null);
-  const [mostrarHoras, setMostrarHoras] = useState(false);
+  const [prediccionHorasComp, setPrediccionHorasComp] = useState(null);
+  const [prediccionHoras, setPrediccionHoras] = useState(null);
+  const [diaPredHoras, setDiaPredHoras] = useState(null)
 
   //Mensajes de error y cargando
   const [cargando, setCargando] = useState(false);
@@ -23,7 +62,7 @@ function App() {
 
   //Cargar provincias desde provincias.json
   useEffect(() => {
-    import("/provincias.json")
+    import("./provincias.json")
     .then((modulo) => {setProvinciasLista(modulo.default);})
     .catch(() => {setError("Error al cargar las provincias");
     });
@@ -56,26 +95,112 @@ function App() {
       });
     }, [provinciaSeleccionada]);
 
-  //FUNCIÓN AL PULSAR EL BOTÓN ACEPTAR
-  const buscarPorNombre = () => {};
-  const buscarPorProvincia = () => {};
+   //Mostrar la fecha en formato: DD/MM
+  function formatearFecha(fechaEntrada) {
+    const fecha = new Date(fechaEntrada);
+    const dia = fecha.getDate().toString().padStart(2, "0");
+    const mes = (fecha.getMonth() + 1).toString().padStart(2, "0");
+    return `${dia}/${mes}`;
+  }
+
+  //Calcular la media de las temperaturas en las franjas horarias para ofrecer una temperatura general
+  //Esto no aparece todos los días, solo los más cercanos
+  function calcularTemperatura(dia) {
+  //En los días en los que no hay datos de temperatura general, no se devuelve nada
+    if(!dia.temperatura.dato || dia.temperatura.dato.length === 0) {
+      return "";
+    }
+    
+    const valoresTemperatura = dia.temperatura.dato.map(t => t.value);
+    const media = Math.round(valoresTemperatura.reduce((a, b) => a + b, 0) / valoresTemperatura.length);
+    return `${media}ºC`;
+  }
+
+
+  //Búsqueda manual por nombre de municipio
+  const busquedaManual = async () => {
+    setError("");
+    setCargando(true);
+    setPrediccionDiaria(null);
+
+    try {
+      const response = await fetch (`http://localhost:3000/api/municipio/nombre/${nombreMunicipio}`);
+      const datos = await response.json();
+
+      if(!datos.success) {
+        setError(datos.error);
+        setCargando(false);
+        return;
+      }
+
+      const idMunicipio = datos.municipio.id;
+
+      const response2 = await fetch(`http://localhost:3000/api/prediccion/${idMunicipio}`);
+      const datos2 = await response2.json();
+
+      if(!datos2.success) {
+        setError(datos2.error);
+        setCargando(false);
+        return;
+      }
+
+      setPrediccionDiaria(datos2.prediccion);
+    }
+    catch(error) {
+      setError("Error al conectar con el servidor");
+    }
+    setCargando(false);
+  }
+
+  //Búsqueda por desplegables
+  const busquedaDesplegables = async () => {
+    setError("");
+    setCargando(true);
+    setPrediccionDiaria(null);
+
+    try {
+      const idMunicipio = municipioSeleccionado;
+
+      if(!idMunicipio) {
+        setError("Debe seleccionar un municipio");
+        setCargando(false);
+        return;
+      }
+
+      const response = await fetch(`http://localhost:3000/api/prediccion/${idMunicipio}`);
+      const datos = await response.json();
+
+      if(!datos.success) {
+        setError(datos.error);
+        setCargando(false);
+        return;
+      }
+
+      setPrediccionDiaria(datos.prediccion);
+    }
+    catch(error) {
+      setError("Error al conectar con el servidor");
+    }
+    setCargando(false);
+  };
+
   return (
     <div className="contenedor">
       <h1 className="titulo">PROYECTO APLICACIÓN DEL TIEMPO</h1>
       <div className="contenedor-busquedas">
 
-        {/*Búsqueda por nombre*/}
-        <div className="div-busqueda">
+        {/*Búsqueda manual*/}
+        <div className="contenedor-busqueda">
           <h3>Búsqueda manual por municipio</h3>
           <input type="text" placeholder="Introduzca un municipio" value={nombreMunicipio} onChange={(e) => setNombreMunicipio(e.target.value)}/>
-          <button onClick={buscarPorNombre}>Aceptar</button>
+          <button onClick={busquedaManual}>Aceptar</button>
         </div>
 
         {/*Separador entre tipos de búsqueda*/}
         <div className="separador"></div>
 
         {/*Búsqueda con desplegables*/}
-        <div className="div-busqueda">
+        <div className="contenedor-busqueda">
           <h3>Búsqueda por provincia y municipio</h3>
           <select value={provinciaSeleccionada} onChange={(e) => setProvinciaSeleccionada(e.target.value)}>
             <option value="">Seleccione la provincia</option>
@@ -91,12 +216,66 @@ function App() {
               <option key={mun.id} value={mun.id}>{mun.nombre}</option>
             ))}
           </select>
-          <button onClick={buscarPorProvincia}>Aceptar</button>
+          <button onClick={busquedaDesplegables}>Aceptar</button>
         </div>
       </div>
-      {/*Mostrar predicción por días*/}
+
+      {/*RESULTADOS*/}
       <div className="contenedor-resultados">
-        
+        {cargando && <p>Cargando...</p>}
+        {error && <p className="error">{error}</p>}
+
+        {/*Predicción por días*/}
+        {prediccionDiaria && (
+          <table className="tabla-prediccion">
+            <tr>
+              <td className="encabezado-vacio"></td>
+              {prediccionDiaria.map((dia, index) => {
+                const fechaFormateada = formatearFecha(dia.fecha);
+                return (<td key={index} className={"celda-fecha"}>{fechaFormateada}</td>);
+              })}
+            </tr>
+            <tr>
+              <td className="encabezado-vacio"></td>
+              {prediccionDiaria.map((dia, index) => {
+                const temperaturaDia = calcularTemperatura(dia);
+                
+                return (<td key={index} className={"temp-dia"}>{temperaturaDia}ºC</td>);
+              })}
+            </tr>
+            <tr>
+              <td className="encabezado-vacio"></td>
+
+              {prediccionDiaria.map((dia, index) => (
+                <td key={index} className="icono-cielo">{iconoDescripcion(dia.estadoCielo)}</td>
+              ))}
+            </tr>
+            <tr>
+              <td className="encabezado">Temperaturas</td>
+              {prediccionDiaria.map((dia, index) => (
+                <td key={index} className="temp">
+                  <span className="temp-min">{dia.temperaturaMin}ºC</span>
+                  <span className="temp-max">{dia.temperaturaMax}ºC</span>
+                </td>
+              ))}
+            </tr>
+            <tr>
+              <td className="encabezado">Precipitaciones</td>
+              {prediccionDiaria.map((dia, index) => (
+                <td key={index}>{dia.probPrecipitacion}%</td>
+              ))}
+            </tr>
+            <tr>
+              <td className="encabezado">Viento</td>
+              {prediccionDiaria.map((dia, index) => (
+                <td key={index} className="viento">
+                  <span>{dia.viento.direccion}</span>
+                  <span>{dia.viento.velocidad} km/h</span>
+                </td>
+              ))}
+            </tr>
+          </table>
+        )}
       </div>
     </div>
   );
